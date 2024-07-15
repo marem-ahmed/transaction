a81cb7d6c58048bcbc01d0e4592a559a877a630d
marem-ahmed <136582570+marem-ahmed@users.noreply.github.com>
2024-07-15 15:45:18 +0300

    displayGraph

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
 import mariam from "../../mariam.json";
 import style from "./DataTable.modules.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  ComposedChart,
+  Bar,
+} from "recharts";
 
 export default function DataTable() {
   const [dataCustomers, setDataCustomers] = useState([]);
   const [dataTransactions, setDataTransactions] = useState([]);
   const [mergedData, setMergedData] = useState([]);
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     setDataCustomers(mariam.customers);
@@ -28,15 +42,36 @@ export default function DataTable() {
     }
   }, [dataCustomers, dataTransactions]);
 
+  useEffect(() => {
+    if (selectedCustomer) {
+      const filterData = dataTransactions.filter(
+        (ele) => ele.customer_id === selectedCustomer.id
+      );
+
+      const totalData = Object.values(
+        filterData.reduce((combine, { date, amount }) => {
+          combine[date] = combine[date] || { date, amount: 0 };
+          combine[date].amount += amount;
+          return combine;
+        }, {})
+      );
+
+      setChartData(totalData);
+    }
+  }, [selectedCustomer, dataTransactions]);
+
   const sortByAmount = () => {
     const sortedData = [...mergedData].sort((x, y) => x.amount - y.amount);
     setMergedData(sortedData);
   };
-const sortByName = () => {
-    const sortedData = [...mergedData].sort((x, y) => x.customer_name.localeCompare(y.customer_name)
+
+  const sortByName = () => {
+    const sortedData = [...mergedData].sort((x, y) =>
+      x.customer_name.localeCompare(y.customer_name)
     );
-        setMergedData(sortedData);
-}
+    setMergedData(sortedData);
+  };
+
   return (
     <div>
       <h1 className="text-center">Customers and Transaction List</h1>
@@ -52,7 +87,7 @@ const sortByName = () => {
           </thead>
           <tbody>
             {mergedData.map((ele) => (
-              <tr key={ele.id}>
+              <tr key={ele.id} onClick={() => setSelectedCustomer(ele)}>
                 <td>{ele.id}</td>
                 <td>{ele.customer_name}</td>
                 <td>{ele.amount}</td>
@@ -64,11 +99,45 @@ const sortByName = () => {
           <button className="btn btn-primary px-5 py-2" onClick={sortByAmount}>
             Sort by Amount
           </button>
-          <button className="btn btn-primary ms-3 px-5 py-2" onClick={sortByName}>
+          <button
+            className="btn btn-primary ms-3 px-5 py-2"
+            onClick={sortByName}
+          >
             Sort by Name
           </button>
         </div>
       </div>
+
+      {selectedCustomer && (
+        <div className="chart-container">
+          <h2 className="text-center">
+            Transaction Amounts for {selectedCustomer.customer_name}
+          </h2>
+          <ResponsiveContainer height={400} className="mx-auto w-75">
+            <ComposedChart data={chartData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis yAxisId="left" orientation="left" />
+              <YAxis yAxisId="right" orientation="right" />
+              <Tooltip />
+              <Legend />
+              <Line
+                yAxisId="left"
+                type="monotone"
+                dataKey="amount"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Bar
+                yAxisId="right"
+                dataKey="amount"
+                barSize={20}
+                fill="#413ea0"
+              />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </div>
+      )}
     </div>
   );
 }
